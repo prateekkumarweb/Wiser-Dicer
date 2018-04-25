df0142d479af2eca0d59395c316e7ae987bbe984
Siddharth Shrivastava <35625973+sid20071997@users.noreply.github.com>
2018-04-26 02:09:11 +0530

    Update Maps.hs

--- src/Maps.hs
+++ src/Maps.hs
@@ -10,3 +10,38 @@ data Level = Level { lMap      :: [String]
        , config   :: [Int]
        , final     :: (Int,Int)
                    } deriving Show
+
+maps = [
+        Level { lMap = ["bbb ",
+                        "bbb ",
+                        "bbb ",
+                        "bbb "]
+              , lStartPos = (0,2)
+        , config = [6,1,3,4,5,2]
+        , final = (2,1)
+              },
+       Level { lMap = [ "b  b",
+                        "b  b",
+                        "bbbb",
+                        "bbbb"]
+              , lStartPos = (0,3)
+        , config = [6,1,4,3,2,5]
+        , final = (1,0)
+              },
+       Level { lMap = [ "bbbb",
+                        "bb b",
+                        "bb b",
+                        "bbbb"]
+              , lStartPos = (0,3)
+        , config = [6,1,3,4,5,2]
+        , final = (0,3)
+              }
+
+  ]
+
+
+getMap level x y =
+  if 0 <= x && x <= 4 &&            
+     0 <= y && y <= 4
+    then (lMap level !! y) !! x  
+    else ' '
