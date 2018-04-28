1cd71e42225abc028e83466c84516ff601f1ace2
Sudhanshu Chawhan <cs16btech11037@iith.ac.in>
2018-04-28 22:18:54 +0530

    one player added

---
+++ src_one_player/Game.hs
@@ -0,0 +1,14 @@
+module Game where
+
+import Data.Array
+import Helper
+
+tileSize :: Float
+tileSize = 100.0
+
+
+
+
+initialGame = initGameForMapLevel 0
+    
+
---
+++ src_one_player/Main.hs
@@ -0,0 +1,18 @@
+module Main where
+
+import Data.Array
+import Graphics.Gloss
+import Graphics.Gloss.Data.Color
+
+import Game
+import Rendering
+import Logic
+
+-- transformGame _ game = game
+
+main :: IO ()
+main = play window backgroundColor 30 initialGame gameAsPicture transformGame (const id)
+ where 
+ 	window = FullScreen
+	backgroundColor = cyan
+	
