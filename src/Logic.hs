e5620cdc4fbd34d84f92bfc814b97485fd3c5c30
Sudhanshu Chawhan <cs16btech11037@iith.ac.in>
2018-04-28 22:49:50 +0530

    Update Logic.hs

--- src/Logic.hs
+++ src/Logic.hs
@@ -2,49 +2,10 @@ module Logic where
 
 import Data.Array
 import Data.Foldable ( asum )
+import Maps
 import Helper
 import Game
 import Graphics.Gloss.Interface.Pure.Game
-
--- isCoordCorrect = inRange ((0, 0), (n - 1, n - 1))
-
--- switchPlayer game =
---     case gamePlayer game of
---       PlayerX -> game { gamePlayer = PlayerO }
---       PlayerO -> game { gamePlayer = PlayerX }
-
--- full :: [Cell] -> Maybe Player
--- full (cell@(Just player):cells) | all (== cell) cells = Just player
--- full _                                                = Nothing
-
--- winner :: Board -> Maybe Player
--- winner board = asum $ map full $ rows ++ cols ++ diags
---     where rows  = [[board ! (i,j) | i <- [0..n-1]] | j <- [0..n-1]]
---           cols  = [[board ! (j,i) | i <- [0..n-1]] | j <- [0..n-1]]
---           diags = [[board ! (i,i) | i <- [0..n-1]]
---                   ,[board ! (i,j) | i <- [0..n-1], let j = n-1-i ]]
-
--- countCells :: Cell -> Board -> Int
--- countCells cell = length . filter ((==) cell) . elems
-
--- checkGameOver game
---     | Just p <- winner board =
---         game { gameState = GameOver $ Just p }
---     | countCells Nothing board == 0 =
---         game { gameState = GameOver Nothing }
---     | otherwise = game
---     where board = gameBoard game
-
--- playerTurn :: Game -> (Int, Int) -> Game
--- playerTurn game cellCoord
---     | isCoordCorrect cellCoord && board ! cellCoord == Nothing =
---         checkGameOver
---         $ switchPlayer
---         $ game { gameBoard = board // [(cellCoord, Just player)] }
---     | otherwise = game
---     where board = gameBoard game
---           player = gamePlayer game
-
 
 -- | If the player choses to move the dice in north direction then congiuration changes
 moveConfigPlayerUp :: ConfigPlayer -> ConfigPlayer
@@ -216,7 +177,7 @@ checkVictory game
 -- | Changes the level of the game
 changeGame :: Int -> Game
 changeGame i 
-	| i == 6 = initGameForMapLevel 5
+	| i == (length maps) = initGameForMapLevel (i-1)
 	| otherwise = initGameForMapLevel i
 
 -- | On pressing space key level is changed
